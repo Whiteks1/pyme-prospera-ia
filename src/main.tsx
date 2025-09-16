d4d80134ca901fc34000188a1afcc56f0514d47c
Leticia <marcelin2219@gmail.com>
2025-09-16 18:16:31 +0200

    Revert "Merge pull request #1 from Whiteks1/feat/posthog"
    This reverts commit 813bcbe37cab048ce426917bf9fa6f5444fd98f0, reversing
    changes made to cff35f2b2268b075fbefae47b624e559f3386e93.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,5 @@
-// src/main.tsx
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { initPostHog, pageview } from "./lib/analytics";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// Inicializar PostHog y registrar el primer pageview
-initPostHog();
-pageview({ path: location.pathname + location.search });
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
